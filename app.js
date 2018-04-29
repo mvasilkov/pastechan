ae3a1ba69c30e41ea383112c6bc06d1fbda37e70
Mark Vasilkov <mvasilkov@gmail.com>
2018-04-29 21:25:44 +0300

    JSON Web Tokens

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const { ObjectId } = require('bson')
 const cmark = require('cmark-emscripten')
 const express = require('express')
 const favicon = require('serve-favicon')
+const jwt = require('jsonwebtoken')
 const logger = require('morgan')
 const nunjucks = require('nunjucks')
 
@@ -9,7 +10,8 @@ const gracefulShutdown = require('./stop')
 const levelup = require('./levelup')
 const nope = require('./nope')
 
-const is_dev = ['development', undefined].includes(process.env.NODE_ENV)
+const dev = ['development', undefined].includes(process.env.NODE_ENV)
+const secret = 'potato'
 
 const app = express()
 const db = levelup(`${__dirname}/LevelDB`, { valueEncoding: 'json' })
@@ -17,15 +19,25 @@ const db = levelup(`${__dirname}/LevelDB`, { valueEncoding: 'json' })
 nunjucks.configure(`${__dirname}/templates`, {
     autoescape: false,
     express: app,
-    watch: is_dev,
+    watch: dev,
 })
 
-app.use(logger(is_dev ? 'dev' : 'short'))
+app.use(logger(dev ? 'dev' : 'short'))
 app.use(favicon(`${__dirname}/static/favicon.ico`))
 app.use('/static', express.static(`${__dirname}/static`, { index: false }))
 
 app.get('/', (req, res) => {
-    res.render('index.html')
+    const objectid = ObjectId()
+    const token = jwt.sign({ salt: objectid.toString(), n: 3 }, secret, { expiresIn: '2 days' })
+    const options = { token }
+    res.format({
+        ['text/html']() {
+            res.render('index.html', options)
+        },
+        ['application/json']() {
+            res.json(options)
+        },
+    })
 })
 
 app.get('/p/:id', (req, res) => {
@@ -55,24 +67,50 @@ app.use(express.json({ strict: true }))
 app.use(express.urlencoded({ extended: false }))
 
 app.post('/p', (req, res) => {
-    const { contents } = req.body
+    const { contents, token } = req.body
     if (!contents) {
         nope.badPageContents(res)
         return
     }
+
+    let decoded
+    try {
+        decoded = jwt.verify(token, secret, { algorithms: ['HS256'] })
+    }
+    catch (err) {
+        nope.badPageContents(res)
+        return
+    }
+
+    db.get(decoded.salt, (err, post) => {
+        if (err) {
+            /* Good path: page not found */
+            if (err.notFound) {
+                savePost(res, contents, decoded)
+                return
+            }
+            /* Database error */
+            nope.cannotSavePage(res)
+            return
+        }
+        /* Page found */
+        nope.badPageContents(res)
+    })
+})
+
+function savePost(res, contents, decoded) {
     const post = {
         contents,
         contents_html: cmark.markdownToHtml(contents, { hardbreaks: true, safe: true, validateUTF8: true }),
     }
-    const objectid = ObjectId()
-    db.put(objectid.toString(), post, err => {
+    db.put(decoded.salt, post, err => {
         if (err) {
             nope.cannotSavePage(res)
             return
         }
-        res.redirect(`/p/${objectid}`)
+        res.redirect(`/p/${decoded.salt}`)
     })
-})
+}
 
 app.use((req, res, next) => {
     nope.pageNotFound(res)
